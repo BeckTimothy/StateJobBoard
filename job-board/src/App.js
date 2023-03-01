1a05a5082ea105cd67fff7e13dd9a999c845411e
BeckTimothy <johndaveleet@gmail.com>
2023-02-28 22:03:41 -0700

    fixed chained promises to resolve issue with race condition

--- job-board/src/App.js
+++ job-board/src/App.js
@@ -8,6 +8,7 @@ function App() {
         judParsed: false,
         legParsed: false,
         execParsed: false,
+        gotJobs: false,
         departments: [],
         positions: []
     }
@@ -16,12 +17,6 @@ function App() {
         ...stateTemplate,
         ...stateData
     };
-    const concatDepartments = (arr) => {
-        let data = localData;
-        data.departments = data.departments.concat(arr);
-        data.departments = [...new Map(data.departments.map((m) => [m.agency, m])).values()]
-        setStateData(data)
-    }
 
     const concatPositions = (arr) => {
         let data = localData;
@@ -31,65 +26,77 @@ function App() {
 
 
     //step 1: build list of departments
-    const getDepartments = async (branch) => {
-        await getEmployeeDepartments("Judicial")
-            .then(()=>{
-                localData.judParsed = true;
-            })
-        await getEmployeeDepartments("Legislature")
-            .then(()=>{
-                localData.legParsed = true;
-            })
-        await getEmployeeDepartments("Executive")
-            .then(()=>{
-                localData.execParsed = true;
-            })
-    }
-
-    const getEmployeeDepartments = async (branch) => {
+    const getAllDepartments = async () => {
         let url  = "https://us-central1-nm-gov-1.cloudfunctions.net/ssp-prod/getEmployeeDepartments";
-
         url += "?operation=getEmployeeDepartments"
-        url += `&branch=${branch}`
+        url += `&branch=Judicial`
         url += `&employeeType=All`
         url += "&year=2023"
         url += "&db=b"
-
-        const response = await fetch(url, {
+        await fetch(url, {
             "method": "GET",
             "Content-Type": "application/x-www-form-urlencoded"
         })
             .then(response => response)
-            .then(data => data)
-
-        let actualResponse = await response.json()
-
-        let data = await JSON.parse(actualResponse.result);
-        data = await data.map(data => {
-            data.branch = branch;
-            return data;
-        });
-        await data.shift();
-
-        switch(branch){
-            case "Judicial":
+            .then(async data => {
                 localData.judParsed = true;
-                break;
-            case "Executive":
-                localData.execParsed = true;
-                break;
-            case "Legislature":
-                localData.legParsed = true;
-                break;
-        }
+                data = await data.json();
+                data = await JSON.parse(data.result);
+                data = await data.map(chunk => {
+                    chunk.branch = 'Judicial';
+                    return chunk;
+                });
+                localData.departments = localData.departments.concat(data);
 
-        concatDepartments(await data);
+                let url  = "https://us-central1-nm-gov-1.cloudfunctions.net/ssp-prod/getEmployeeDepartments";
+                url += "?operation=getEmployeeDepartments"
+                url += `&branch=Legislature`
+                url += `&employeeType=All`
+                url += "&year=2023"
+                url += "&db=b"
+                await fetch(url, {
+                    "method": "GET",
+                    "Content-Type": "application/x-www-form-urlencoded"
+                })
+                    .then(response => response)
+                    .then(async data => {
+                        localData.legParsed = true;
+                        data = await data.json();
+                        data = await JSON.parse(data.result);
+                        data = await data.map(chunk => {
+                            chunk.branch = 'Legislature';
+                            return chunk;
+                        });
+                        localData.departments = localData.departments.concat(data);
+
+                        let url  = "https://us-central1-nm-gov-1.cloudfunctions.net/ssp-prod/getEmployeeDepartments";
+                        url += "?operation=getEmployeeDepartments"
+                        url += `&branch=Executive`
+                        url += `&employeeType=All`
+                        url += "&year=2023"
+                        url += "&db=b"
+                        await fetch(url, {
+                            "method": "GET",
+                            "Content-Type": "application/x-www-form-urlencoded"
+                        })
+                            .then(response => response)
+                            .then(async data => {
+                                localData.execParsed = true;
+                                data = await data.json();
+                                data = await JSON.parse(data.result);
+                                data = await data.map(chunk => {
+                                    chunk.branch = 'Executive';
+                                    return chunk;
+                                });
+                                localData.departments = localData.departments.concat(await data);
+                                setStateData(localData)
+                            })
+                    })
+            })
     }
 
     useEffect( ()=>{
-        getEmployeeDepartments("Judicial")
-        getEmployeeDepartments("Executive")
-        getEmployeeDepartments("Legislature")
+        getAllDepartments();
     }, [])
 
     //step 2: use list of departments to build list of positions
@@ -103,7 +110,6 @@ function App() {
         url += `&businessUnit=${unit}`
         url += "&db=b"
 
-        //const response =
         await fetch(url, {
             "method": "GET",
             "Content-Type": "application/x-www-form-urlencoded"
@@ -130,6 +136,7 @@ function App() {
 
         if (localData.departments.length > 0) {
             let department = localData.departments.shift();
+            if (localData.departments.length === 0) {localData.gotJobs = true;}
             getPosition(department.branch, department.businessUnit)
         }
     }
@@ -140,15 +147,13 @@ function App() {
     //sort localData by salary midpoint
     localData.positions = localData.positions?.sort((x,y) => {return Number(x.positionMidPoint) - Number(y.positionMidPoint)})
 
-
-
     localData.positions = localData.positions?.filter(x => x.position.toLowerCase().includes('project manager'))
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>peeeeeeeeep</p>
-          <button onClick={()=>{parseDepartment()}}> Populate Vacant NM State Government Positions  </button>
+          <h1>State of New Mexico: Funded and Vacant Positions</h1>
+          {localData.gotJobs ? <></>: <><p>This message will disappear when all data has been received...</p> </>}
           <span>{localData.positions?.length} vacant state positions</span>
           <div className={'positionBar thick'}>
               <span className={'spannum'}>{`Salary MidPoint`}</span>
